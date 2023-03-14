a2fc97bce29c6ed44068278a4fe5b28f8b4af3d0
oscarCastroC <followd2021@gmail.com>
2023-03-14 11:41:00 +0100

    Update UIMainScene.cs

--- Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
+++ Junior-Programmer-Starter-Files/Assets/Scripts/UI/UIMainScene.cs
@@ -73,4 +73,9 @@ public class UIMainScene : MonoBehaviour
             InfoPopup.Name.text = content.GetName();
         }
     }
+
+    public void goBackMenu()
+    {
+        SceneManager.LoadScene(0);  // Menu
+    }
 }
